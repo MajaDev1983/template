9a7c9f60a3ff6bb31afa78866f737e6f6d31635a
MajaDev1983 <majastipetic22@gmail.com>
2019-06-29 13:40:59 +0200

    template

---
+++ Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template <typename T>
+T Sum(T a, T b)
+{
+	return a + b;
+}
+
+template <class _Ty>
+class MyArray
+{
+public:
+	_Ty table[];
+
+	void push_back(_Ty element);
+};
+
+MyArray::MyArray()
+{
+}
+
+MyArray::~MyArray()
+{
+}
+
+int main()
+{
+	int sumNumber = Sum(5, 7);
+	float floatingSum = Sum(3.4f, 7.7f);
+	std::cout << "Zbroj intova: " << sumNumber << std::endl;
+	std::cout << "Zbroj floatova: " << floatingSum << std::endl;
+
+	std::string first = "Maja";
+	std::string second = "dev";
+	std::string someString = Sum(first, second);
+	std::cout << "Zbroj stringova: " << someString << std::endl;
+
+	std::vector<int> vectorTemplate;
+
+	std::cin.get();
+}
